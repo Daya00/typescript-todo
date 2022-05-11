9493f7f58ce6008eb905004241c03d02e185d5a4
Daya-Redux <it.0905613036@gmail.com>
2022-05-11 12:07:53 +0530

    skkipped

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
-function App() {
+
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    debugger;
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField handleAdd={handleAdd} todo={todo} setTodo={setTodo} />
+      <TodoList todos={todos} setTodos={setTodos} />
+     
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// let name: string;
+// let age: number;
+
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];//array
+// let role: [number, string];//tupel
+// let personeName: unknown;
+
+// let printName: (name: string) => never;// nothing return;
+// let printName1: (name: string) => void; //return undefined;
+
+
+// age = 20;
+
+// type Person = {
+//   name: string,
+//   age: number
+// }
+
+// type X = Person & {
+//   x1: string,
+//   x2: number
+// }
+
+// let y: X = {
+//   name: "data",
+//   age: 10,
+//   x1: "daya",
+//   x2: 21
+// }
+
+// interface Person2 {
+//   p1: string,
+//   p2: number
+// }
+
+// interface Person1 extends Person2 {
+//   name1: string,
+//   age1: number
+// }
+
+// let NewPerson : Person1 &{
+//   p1:"daya",
+//   p2:11,
+//   name1:"dasya",
+//   age1:11, 
+// }
